fc9efd965a42a511c93334d3b0423694fec46265
Mauro Abidal Carrer <mauroabidal@yahoo.fr>
2025-07-02 19:08:49 +0200

    Feat: Added module to load the datasets stats.

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 # dataset
 TEST_DATASET_HANDLE = "egortrushin/open-wfi-test"
 TRAIN_VALIDAION_DATASET_HANDLE = "brendanartley/openfwi-preprocessed-72x72"
+DATASET_STATS_HANDLE = "mauroabidalcarrer/open-fwi-stats"
 SAMPLES_PER_NPY_FILE = 500
 EPSILON = 1e-8
 # model
